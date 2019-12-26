8ec12e667aee37eb65c5bf48a8fcc4780ca2fcb9
graemebrooks <graemebrooks@gmail.com>
2019-12-26 15:46:21 -0600

    fixes signup function indentation

--- main_app/views.py
+++ main_app/views.py
@@ -249,12 +249,12 @@ def signup(request):
     error_message = ''
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-    if form.is_valid():
-        user = form.save()
-        login(request, user)
-        return redirect('home')
-    else:
-        error_message = 'Invalid sign up - try again'
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
